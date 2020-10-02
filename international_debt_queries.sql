a6a5e75d69c0ef25d4dd68edecbdab84a73be695
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-10-01 17:27:49 -0700

    income_table queries

--- international_debt_queries.sql
+++ international_debt_queries.sql
@@ -120,3 +120,43 @@ LIMIT 1;
 
 
 
+-- TABLE income_table
+
+-- What are the top 10 type of debts in 2019 for each income level
+SELECT indicator_name, year_2019 FROM income_table
+WHERE year_2019 IS NOT NULL
+GROUP BY indicator_name, year_2019
+ORDER BY year_2019 DESC;
+
+
+-- For the top type of debt in 2019, where does each income level stand?
+SELECT country_name, year_2019 FROM income_table
+WHERE indicator_name = 'Principal repayments on external debt, long-term (AMT, current US$)'
+ORDER BY year_2019 DESC;
+
+-- Which income level has the highest debt? Lowest?
+SELECT country_name, SUM(year_1970+year_1971+year_1972+year_1973+year_1974+year_1975+
+	year_1976+year_1977+year_1978+year_1979+year_1980+year_1981+year_1982+year_1983+
+	year_1984+year_1985+year_1986+year_1987+year_1988+year_1989+year_1990+year_1991+
+	year_1992+year_1993+year_1994+year_1995+year_1996+	year_1997+year_1998+
+	year_1999+year_2000+year_2001+year_2002+year_2003+year_2004+year_2005+year_2006+	
+	year_2007+year_2008+year_2009+year_2010+year_2011+year_2012+year_2013+year_2014+
+	year_2015+year_2016+year_2017+year_2018+year_2019) 
+AS total_debt FROM income_table
+GROUP BY country_name
+ORDER BY total_debt DESC
+LIMIT 1;
+
+
+SELECT country_name, SUM(year_1970+year_1971+year_1972+year_1973+year_1974+year_1975+
+	year_1976+year_1977+year_1978+year_1979+year_1980+year_1981+year_1982+year_1983+
+	year_1984+year_1985+year_1986+year_1987+year_1988+year_1989+year_1990+year_1991+
+	year_1992+year_1993+year_1994+year_1995+year_1996+	year_1997+year_1998+
+	year_1999+year_2000+year_2001+year_2002+year_2003+year_2004+year_2005+year_2006+	
+	year_2007+year_2008+year_2009+year_2010+year_2011+year_2012+year_2013+year_2014+
+	year_2015+year_2016+year_2017+year_2018+year_2019) 
+AS total_debt FROM income_table
+GROUP BY country_name
+ORDER BY total_debt
+LIMIT 1;
+
