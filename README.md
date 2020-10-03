6b7158707471bff0c1ff7209243dbc8fc7f51d2c
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-10-03 01:59:10 -0700

    fixing queries

--- README.md
+++ README.md
@@ -27,12 +27,12 @@ with SQL queries and visualizing analysis with Tableau
 ## SQL Analysis
 **Income Level**
 > Income levels were Low income, lower middle income,  middle income, upper middle income
-- "PPG, Private Creditors (AMT, Current US$)" was the top type of debt owed with middle income being affected the most in 2019 - [PPG defined Source: The World Bank](https://datacatalog.worldbank.org/ppg-private-creditors-amt-current-us-0)
-- Middle Income Countries have per capita gross national income of US$1,026 to $12,475 (2011) - [Source: The World Bank](https://www.worldbank.org/en/country/mic/overview#:~:text=The%20world's%20Middle%20Income%20Countries,%244%2C046%20and%20%2412%2C535%20(2021))
+<!-- - "PPG, Private Creditors (AMT, Current US$)" was the top type of debt owed with middle income being affected the most in 2019 - [PPG defined Source: The World Bank](https://datacatalog.worldbank.org/ppg-private-creditors-amt-current-us-0) -->
+<!-- - Middle Income Countries have per capita gross national income of US$1,026 to $12,475 (2011) - [Source: The World Bank](https://www.worldbank.org/en/country/mic/overview#:~:text=The%20world's%20Middle%20Income%20Countries,%244%2C046%20and%20%2412%2C535%20(2021)) -->
 
 **Countries**
 > Only 122 countries in dataset out of 195 countries
-- "PPG, official creditors (DIS, current US$)" was the top type of debt owed in 2019 - [PPG Defined Source: The World Bank](https://datacatalog.worldbank.org/search?query=%20ppg-official-creditors-dis-current-us-1)
-- Mexico has the highest debt and Timor-Leste in Southeast Asia had the lowest debt
+<!-- - "PPG, official creditors (DIS, current US$)" was the top type of debt owed in 2019 - [PPG Defined Source: The World Bank](https://datacatalog.worldbank.org/search?query=%20ppg-official-creditors-dis-current-us-1) -->
+<!-- - Mexico has the highest debt and Timor-Leste in Southeast Asia had the lowest debt -->
 
 
--- international_debt_queries.sql
+++ international_debt_queries.sql
@@ -11,7 +11,7 @@ ORDER BY country_name ;
 DELETE FROM international_debt
 WHERE country_name IN ('IDA only','Bosnia and Herzegovina', 'East Asia & Pacific (excluding high income)',
  'Europe & Central Asia (excluding high income)', 'Latin America & Caribbean (excluding high income)',
- 'Middle East & North Africa (excluding high income)','Sub-Saharan Africa (excluding high income)');
+ 'Middle East & North Africa (excluding high income)','Sub-Saharan Africa (excluding high income)', 'South Asia');
 
 -- Removing repayment, payment, disbursements interested in debts owed
 DELETE FROM international_debt
@@ -36,8 +36,8 @@ WHERE country_name IN ('Low & middle income');
 -- Removing repayment, payment, disbursements interested in debts owed
 DELETE FROM income_table
 WHERE indicator_name LIKE ('%repayments%')
-AND indicator_name LIKE ('%payment%')
-AND indicator_name LIKE ('%Disbursements%');
+OR indicator_name LIKE ('%payment%')
+OR indicator_name LIKE ('%Disbursements%');
 
 -- Create new table with 
 -- Least developed countries: UN classification
@@ -89,6 +89,8 @@ GROUP BY country_name, indicator_name, year_2019
 ORDER BY year_2019 DESC
 LIMIT 10;
 
+-- 
+
 
 --  Total Debt
 SELECT country_name, SUM(year_1970+year_1971+year_1972+year_1973+year_1974+year_1975+
@@ -103,33 +105,43 @@ GROUP BY country_name
 ORDER BY total_debt DESC;
 
 
--- Which country has the highest debt? Lowest?
-SELECT country_name, SUM(year_1970+year_1971+year_1972+year_1973+year_1974+year_1975+
-	year_1976+year_1977+year_1978+year_1979+year_1980+year_1981+year_1982+year_1983+
-	year_1984+year_1985+year_1986+year_1987+year_1988+year_1989+year_1990+year_1991+
-	year_1992+year_1993+year_1994+year_1995+year_1996+	year_1997+year_1998+
-	year_1999+year_2000+year_2001+year_2002+year_2003+year_2004+year_2005+year_2006+	
-	year_2007+year_2008+year_2009+year_2010+year_2011+year_2012+year_2013+year_2014+
-	year_2015+year_2016+year_2017+year_2018+year_2019) 
-AS total_debt FROM international_debt
-GROUP BY country_name
-ORDER BY total_debt DESC
-LIMIT 1;
+-- -- Which country has the highest debt? Lowest?
+-- SELECT country_name, SUM(year_1970+year_1971+year_1972+year_1973+year_1974+year_1975+
+-- 	year_1976+year_1977+year_1978+year_1979+year_1980+year_1981+year_1982+year_1983+
+-- 	year_1984+year_1985+year_1986+year_1987+year_1988+year_1989+year_1990+year_1991+
+-- 	year_1992+year_1993+year_1994+year_1995+year_1996+	year_1997+year_1998+
+-- 	year_1999+year_2000+year_2001+year_2002+year_2003+year_2004+year_2005+year_2006+	
+-- 	year_2007+year_2008+year_2009+year_2010+year_2011+year_2012+year_2013+year_2014+
+-- 	year_2015+year_2016+year_2017+year_2018+year_2019) 
+-- AS total_debt FROM international_debt
+-- GROUP BY country_name
+-- ORDER BY total_debt DESC
+-- LIMIT 1;
 
 
-SELECT country_name, SUM(year_1970+year_1971+year_1972+year_1973+year_1974+year_1975+
-	year_1976+year_1977+year_1978+year_1979+year_1980+year_1981+year_1982+year_1983+
-	year_1984+year_1985+year_1986+year_1987+year_1988+year_1989+year_1990+year_1991+
-	year_1992+year_1993+year_1994+year_1995+year_1996+	year_1997+year_1998+
-	year_1999+year_2000+year_2001+year_2002+year_2003+year_2004+year_2005+year_2006+	
-	year_2007+year_2008+year_2009+year_2010+year_2011+year_2012+year_2013+year_2014+
-	year_2015+year_2016+year_2017+year_2018+year_2019) 
-AS total_debt FROM international_debt
-GROUP BY country_name
-ORDER BY total_debt 
-LIMIT 1;
+-- SELECT country_name, SUM(year_1970+year_1971+year_1972+year_1973+year_1974+year_1975+
+-- 	year_1976+year_1977+year_1978+year_1979+year_1980+year_1981+year_1982+year_1983+
+-- 	year_1984+year_1985+year_1986+year_1987+year_1988+year_1989+year_1990+year_1991+
+-- 	year_1992+year_1993+year_1994+year_1995+year_1996+	year_1997+year_1998+
+-- 	year_1999+year_2000+year_2001+year_2002+year_2003+year_2004+year_2005+year_2006+	
+-- 	year_2007+year_2008+year_2009+year_2010+year_2011+year_2012+year_2013+year_2014+
+-- 	year_2015+year_2016+year_2017+year_2018+year_2019) 
+-- AS total_debt FROM international_debt
+-- GROUP BY country_name
+-- ORDER BY total_debt 
+-- LIMIT 1;
 
-
+-- -- Find the highest debt in each county. Lowest?
+-- SELECT country_name, MAX(GREATEST(year_1970,year_1971,year_1972,year_1973,year_1974,year_1975,
+-- 	year_1976,year_1977,year_1978,year_1979,year_1980,year_1981,year_1982,year_1983,
+-- 	year_1984,year_1985,year_1986,year_1987,year_1988,year_1989,year_1990,year_1991,
+-- 	year_1992,year_1993,year_1994,year_1995,year_1996, year_1997,year_1998,
+-- 	year_1999,year_2000,year_2001,year_2002,year_2003,year_2004,year_2005,year_2006,	
+-- 	year_2007,year_2008,year_2009,year_2010,year_2011,year_2012,year_2013,year_2014,
+-- 	year_2015,year_2016,year_2017,year_2018,year_2019)) AS debt
+-- 	FROM international_debt
+-- 	GROUP BY country_name
+-- 	ORDER BY debt DESC;
 
 
 -- TABLE income_table
@@ -146,29 +158,32 @@ SELECT country_name, year_2019 FROM income_table
 WHERE indicator_name = 'PPG, private creditors (AMT, current US$)'
 ORDER BY year_2019 DESC;
 
--- Which income level has the highest debt? Lowest?
-SELECT country_name, SUM(year_1970+year_1971+year_1972+year_1973+year_1974+year_1975+
-	year_1976+year_1977+year_1978+year_1979+year_1980+year_1981+year_1982+year_1983+
-	year_1984+year_1985+year_1986+year_1987+year_1988+year_1989+year_1990+year_1991+
-	year_1992+year_1993+year_1994+year_1995+year_1996+	year_1997+year_1998+
-	year_1999+year_2000+year_2001+year_2002+year_2003+year_2004+year_2005+year_2006+	
-	year_2007+year_2008+year_2009+year_2010+year_2011+year_2012+year_2013+year_2014+
-	year_2015+year_2016+year_2017+year_2018+year_2019) 
-AS total_debt FROM income_table
-GROUP BY country_name
-ORDER BY total_debt DESC
-LIMIT 1;
+-- -- Which income level has the highest debt? Lowest?
+-- SELECT country_name, SUM(year_1970+year_1971+year_1972+year_1973+year_1974+year_1975+
+-- 	year_1976+year_1977+year_1978+year_1979+year_1980+year_1981+year_1982+year_1983+
+-- 	year_1984+year_1985+year_1986+year_1987+year_1988+year_1989+year_1990+year_1991+
+-- 	year_1992+year_1993+year_1994+year_1995+year_1996+	year_1997+year_1998+
+-- 	year_1999+year_2000+year_2001+year_2002+year_2003+year_2004+year_2005+year_2006+	
+-- 	year_2007+year_2008+year_2009+year_2010+year_2011+year_2012+year_2013+year_2014+
+-- 	year_2015+year_2016+year_2017+year_2018+year_2019) 
+-- AS total_debt FROM income_table
+-- GROUP BY country_name
+-- ORDER BY total_debt DESC
+-- LIMIT 1;
 
 
-SELECT country_name, SUM(year_1970+year_1971+year_1972+year_1973+year_1974+year_1975+
-	year_1976+year_1977+year_1978+year_1979+year_1980+year_1981+year_1982+year_1983+
-	year_1984+year_1985+year_1986+year_1987+year_1988+year_1989+year_1990+year_1991+
-	year_1992+year_1993+year_1994+year_1995+year_1996+	year_1997+year_1998+
-	year_1999+year_2000+year_2001+year_2002+year_2003+year_2004+year_2005+year_2006+	
-	year_2007+year_2008+year_2009+year_2010+year_2011+year_2012+year_2013+year_2014+
-	year_2015+year_2016+year_2017+year_2018+year_2019) 
-AS total_debt FROM income_table
-GROUP BY country_name
-ORDER BY total_debt
-LIMIT 1;
+-- SELECT country_name, SUM(year_1970+year_1971+year_1972+year_1973+year_1974+year_1975+
+-- 	year_1976+year_1977+year_1978+year_1979+year_1980+year_1981+year_1982+year_1983+
+-- 	year_1984+year_1985+year_1986+year_1987+year_1988+year_1989+year_1990+year_1991+
+-- 	year_1992+year_1993+year_1994+year_1995+year_1996+	year_1997+year_1998+
+-- 	year_1999+year_2000+year_2001+year_2002+year_2003+year_2004+year_2005+year_2006+	
+-- 	year_2007+year_2008+year_2009+year_2010+year_2011+year_2012+year_2013+year_2014+
+-- 	year_2015+year_2016+year_2017+year_2018+year_2019) 
+-- AS total_debt FROM income_table
+-- GROUP BY country_name
+-- ORDER BY total_debt
+-- LIMIT 1;
+
+
+
 
